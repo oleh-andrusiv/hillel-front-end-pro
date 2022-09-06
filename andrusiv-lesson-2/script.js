05af77f029ec7258670c7998772a0208ed180d44
oleh-andrusiv <andrusiv.o.m@gmail.com>
2022-09-06 11:53:08 +0300

    lesson-2 files added

---
+++ andrusiv-lesson-2/script.js
@@ -0,0 +1,19 @@
+let myName = "Oleh";
+console.log(myName);
+let mySurname = "Andrusiv";
+console.log(mySurname);
+let myCity = "Kyiv"; // become my native after more than 10 years of living there
+console.log(myCity);
+/* The best way to console.log the values/results is like above, or beter add some comments to each, like 
+console.log (myCity + " become my native after more than 10 years of living there")? */
+let someAverageNumber = (4 + 8 + 2 + 6) / 4;
+console.log(everageNumber);
+let circleRadius = 12;
+console.log(circleRadius);
+let circleLenght = 2 * 3.14 * circleRadius;
+console.log(circleLenght);
+let circleSquare = 3.14 * (circleRadius ** 2);
+console.log(circleSquare);
+// let 1 = Null;
+// let *-/ = Gap;
+// let if = 12;
